50093d8da05684509d0460c3f6de3d37be4ead7a
Kasunki <kasunki.samarasekara@student.hamk.fi>
2023-11-04 19:29:07 +0200

    Initial commit

---
+++ src/Assignment1.java
@@ -0,0 +1,44 @@
+
+public class Assignment1 {
+
+	public static void main(String[] args) {
+		 int number1 = 10;
+		 int number2 = 3;
+		 number1 = 50;
+		 number2 = 5;
+		 
+	 System.out.println("Printer App");
+	 System.out.print("This app is made by:");
+         
+	 String creator = "Kasunki";
+	 System.out.println(creator);
+	 
+//	 System.out.println(number1);
+	 System.out.println("The value of number1 is "+ number1);
+	 System.out.println("The value of number2 is "+ number2);
+	 
+	 int multiplication = number1 * number2; 
+	 System.out.println(multiplication);
+	 System.out.println(number1 + " * " + number2 + " = "+ multiplication);
+	 
+	 int addition = number1 + number2; 
+	 System.out.println(number1 + " + " + number2 + " = "+ addition);
+	 
+	 int substraction = number1 - number2; 
+	 System.out.println(number1 + " - " + number2 + " = "+ substraction);
+	 
+	 double division = number1 / number2; 
+	 System.out.println(number1 + " / " + number2 + " = " + division);
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
